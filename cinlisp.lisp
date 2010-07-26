1dd60596dea254533036d2c30681b920a7cc523b
Mauro Ciancio <maurociancio@gmail.com>
2010-07-26 15:19:05 -0300

    parsear asignaciones

--- cinlisp.lisp
+++ cinlisp.lisp
@@ -1,7 +1,47 @@
 ;autor mauro ciancio
 
 (defun exec (code input &optional (memory nil))
-	nil
+	(if (null code)
+		nil
+		(cond
+		;parseamos variables globales
+			(eq (caar code) 'int)
+			(exec (cdr code) input (add_global_var memory (car code)))
+
+		;main
+			(t nil)
+		)
+	)
+)
+
+(defun exec_main (code input &optional (memory nil))
+)
+
+;agregar una variable global a la memoria
+;memory: ( (globales) (stack) )
+; (globales) = ( (a 1) (b 2) (c 3) ... )
+; (stack) = ( ( (a 1) (b 2) ) ( (c 4) ))
+;code: 	int a = 0
+;	int b
+(defun add_global_var (memory code)
+	;si la mem es null la inicializamos
+	(if (null memory)
+		(list (list) (list))
+	)
+)
+
+;code: 	int a = 0
+;	int b
+(defun parse_assignment (code)
+	(if (null code)
+		nil
+		(if (eq (length code) 2)
+			;caso sin valor inicial
+			(list (cadr code) 0)
+			;caso con valor inicial
+			(list (cadr code) (nth 3 code))
+		)
+	)
 )
 
 ;=============================
@@ -19,3 +59,7 @@
 ;tests
 ;=============================
 (test 'exec-null (exec nil nil) nil)
+
+(test 'parse-assign1 (parse_assignment '(int a)) '(a 0))
+(test 'parse-assign2 (parse_assignment '(int b)) '(b 0))
+(test 'parse-assign3 (parse_assignment '(int c = 1)) '(c 1))
