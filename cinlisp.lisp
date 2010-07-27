3260fb88339065f6a7d26d5344aa839f7023fa0c
Mauro Ciancio <maurociancio@gmail.com>
2010-07-27 00:42:27 -0300

    exists var

--- cinlisp.lisp
+++ cinlisp.lisp
@@ -26,7 +26,24 @@
 (defun add_global_var (memory code)
 	;si la mem es null la inicializamos
 	(if (null memory)
-		(list (list) (list))
+		(list (list (parse_assignment code)) (list))
+		(if (exists_var (cadr code) (car memory))
+			nil
+			nil
+		)
+	)
+)
+
+;verifica que var este en la memoria
+;memory: ( (a 1) (b 2) (c 3) )
+;var: a
+(defun exists_var (var memory)
+	(if (null memory)
+		nil
+		(if (eq (caar memory) var)
+			t
+			(exists_var var (cdr memory))
+		)
 	)
 )
 
@@ -63,3 +80,18 @@
 (test 'parse-assign1 (parse_assignment '(int a)) '(a 0))
 (test 'parse-assign2 (parse_assignment '(int b)) '(b 0))
 (test 'parse-assign3 (parse_assignment '(int c = 1)) '(c 1))
+
+(test 'add-global-var1 (add_global_var nil '(int c = 1)) '(
+							((c 1))
+							()
+							))
+
+;(test 'add-global-var2 (add_global_var '( ((a 2)) () ) '(int c = 1)) '(
+;							((c 1)(a 2))
+;							()
+;							))
+
+(test 'exists-var1 (exists_var 'a nil) nil)
+(test 'exists-var2 (exists_var 'a '( (a 1) (b 2) )) t)
+(test 'exists-var3 (exists_var 'c '( (a 1) (b 2) )) nil)
+(test 'exists-var4 (exists_var 'b '( (a 1) (b 2) )) t)
